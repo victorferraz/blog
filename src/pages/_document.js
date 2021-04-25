7786223d5d82b66befbfb9294a4d043837dcc763
Victor <cashrox@gmail.com>
2021-04-25 16:53:05 -0300

    2Fix/analyticcs (#8)
    * add analytics
    
    * fix analytics
    
    * fix document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -41,12 +41,14 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css?family=Spartan&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Archivo|Archivo+Black&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet"></link>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-24324707-1"></script>
           <script dangerouslySetInnerHTML={{__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-2C95CEMJM7');
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            
+            ga('create', 'UA-24324707-1', 'auto');
+            ga('send', 'pageview');
           `}}>
           </script>
         </Head>
